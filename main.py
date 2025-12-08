e6dbe79aa80abc5ae4e6def1759e9550ad3543b2
Trytias <felixhartzell2010@gmail.com>
2025-12-08 13:25:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 #Variables
 piece_active = False
 current_piece = 0
+main_block_cords = 0
+map = 0
 
 #Define piece types and their block positions
 #1 = T piece
@@ -13,42 +15,63 @@ current_piece = 0
 #6 = S piece
 #7 = Mirrored S piece
 piece_types = {
-    1: [(-1, 0), (0, 1), (1, 0)],
-    2: [(0, 1), (0, -1), (0, -2)],
-    3: [(0, 1), (1, 0), (2, 0)],
-    4: [(0, 1), (-1, 0), (-2, 0)],
-    5: [(1, 0), (0, 1), (1, 1)],
-    6: [(0, -1), (1, 0), (1, 1)],
-    7: [(0, -1), (-1, 0), (-1, 1)]
+    "1": [(-1, 0), (0, 1), (1, 0)],
+    "2": [(0, 1), (0, -1), (0, -2)],
+    "3": [(0, 1), (1, 0), (2, 0)],
+    "4": [(0, 1), (-1, 0), (-2, 0)],
+    "5": [(1, 0), (0, 1), (1, 1)],
+    "6": [(0, -1), (1, 0), (1, 1)],
+    "7": [(0, -1), (-1, 0), (-1, 1)]
 }
 
 #Make the map
 def make_map():
-    rows = {}
-    for i in range(20):
-        rows[i] = []
-    print(rows)
+    global map
+    map = [[0] * 10 for _ in range(20)]
+    print(map)
 make_map()
 
 def assign_piece():
     #Assign piece to the main point
     global current_piece
-    current_piece = piece_types[random.randint(1, 7)]
+    current_piece = piece_types[f"{random.randint(1, 7)}"]
     print(current_piece)
     assign_global_coords()
 
 def assign_global_coords():
     print (current_piece)
+    global main_block_cords
     main_block_cords = {
-    1: [5, 19],
-    2: [5 + current_piece[0][0], 19 + current_piece[0][1]],
-    3: [5 + current_piece[1][0], 19 + current_piece[1][1]],
-    4: [5 + current_piece[2][0], 19 + current_piece[2][1]],
+    "1": [5, 19],
+    "2": [5 + current_piece[0][0], 19 + current_piece[0][1]],
+    "3": [5 + current_piece[1][0], 19 + current_piece[1][1]],
+    "4": [5 + current_piece[2][0], 19 + current_piece[2][1]],
     }
     print(main_block_cords)
+    main_frame_loop()
 
-def main_frame_loop():
+def check_piece_active():
     if not piece_active:
         assign_piece()
 
-main_frame_loop()
+def main_frame_loop():
+    for i in range(4):
+        main_block_cords[f"{i + 1}"][1] -= 1
+        print(main_block_cords)
+    for f in range(20):
+        for i in range(10):
+            checking = map[f][i]
+            if checking == 1:
+                for t in range(4):
+                    if main_block_cords[f"{t + 1}"][0] == i:
+                        if main_block_cords[f"{t + 1}"][1] == f:
+                            print("low_taper_fade")
+    for j in range(4):
+        if main_block_cords[f"{j + 1}"][1] == 0:
+            stop_it()
+    main_frame_loop()
+
+def stop_it():
+    print("john_pork")
+
+check_piece_active()
