9499d294bcc452ffe5781cd4910120fcd0ec9838
Trytias <felixhartzell2010@gmail.com>
2025-12-08 14:12:58 +0100

    added input handling

--- main.py
+++ main.py
@@ -35,11 +35,10 @@ def assign_piece():
     #Assign piece to the main point
     global current_piece
     current_piece = piece_types[f"{random.randint(1, 7)}"]
-    print(current_piece)
     assign_global_coords()
 
 def assign_global_coords():
-    print (current_piece)
+    print (f"\033[91m{current_piece}\033[91m")
     global main_block_cords
     main_block_cords = {
     "1": [5, 19],
@@ -47,7 +46,6 @@ def assign_global_coords():
     "3": [5 + current_piece[1][0], 19 + current_piece[1][1]],
     "4": [5 + current_piece[2][0], 19 + current_piece[2][1]],
     }
-    print(main_block_cords)
     main_frame_loop()
 
 def check_piece_active():
@@ -55,9 +53,15 @@ def check_piece_active():
         assign_piece()
 
 def main_frame_loop():
+    user_input = input("Input(a/d): ").lower()
+    if user_input == "a":
+        choice = -1
+    else:
+        choice = 1
+    for i in range(4):
+        main_block_cords[f"{i + 1}"][0] += choice
     for i in range(4):
         main_block_cords[f"{i + 1}"][1] -= 1
-        print(main_block_cords)
     for f in range(20):
         for i in range(10):
             checking = map[f][i]
@@ -65,13 +69,24 @@ def main_frame_loop():
                 for t in range(4):
                     if main_block_cords[f"{t + 1}"][0] == i:
                         if main_block_cords[f"{t + 1}"][1] == f:
-                            print("low_taper_fade")
+                            go_back_twin()
+
     for j in range(4):
         if main_block_cords[f"{j + 1}"][1] == 0:
             stop_it()
     main_frame_loop()
 
-def stop_it():
-    print("john_pork")
+def go_back_twin():
+    for i in range(4):
+        main_block_cords[f"{i + 1}"][1] += 1
+    stop_it()
 
+def stop_it():
+    for i in range(4):
+        map[main_block_cords[f"{i + 1}"][1]][main_block_cords[f"{i + 1}"][0]] = 1
+    map_printing()
+    check_piece_active()
+def map_printing():
+    for i in range(20):
+        print(f"\033[92m{map[19 - i]}\033[92m")
 check_piece_active()
